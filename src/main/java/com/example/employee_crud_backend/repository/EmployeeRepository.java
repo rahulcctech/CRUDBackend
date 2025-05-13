85d78ba498b10a58c66a364286848700a4184c88
rahulcctech <rahul.roy@cctech.co.in>
2025-05-13 23:34:32 +0530

    Revert "Initial commit"
    This reverts commit ff35eb91f840e6e5aa4759f21c3da06724150703.

--- src/main/java/com/example/employee_crud_backend/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.employee_crud_backend.repository;
-
-import com.example.employee_crud_backend.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
